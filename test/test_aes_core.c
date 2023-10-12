259aed804754c44aa0334f788ff55a192ea61d7c
bmh <27549756+bmh-gh@users.noreply.github.com>
2023-10-12 19:11:36 +0200

    copy mistaken code in test_aes_core.h to test_aes_core.c

--- test/test_aes_core.c
+++ test/test_aes_core.c
@@ -1,3 +1,39 @@
 //
 // Created by bmh on 12.10.23.
 //
+#include <stdbool.h>
+const char *group = "AES_CORE";
+
+// TODO: Add test cases given by FIPS 197 (AES)
+// TODO: Correct the Messages in the assert statements to show why the assert went wrong
+bool aes_key_addition(void) {
+    return assert_true(true, "Key Addition")
+}
+
+bool aes_shift_rows(void) {
+    return assert_true(true, "Shift Rows");
+}
+
+bool aes_mix_columns(void) {
+    return assert_true(true, "Mix Columns");
+}
+
+bool aes_sub_bytes(void) {
+    return assert_true(true, "Substitute Bytes");
+}
+
+bool key_schedule(void) {
+    return assert_true(true, "Key Schedule");
+}
+
+void test_aes_core() {
+    start_tests(group);
+    // TODO: Add test-runner here:
+    run_test("Key Addition", aes_key_addition);
+    run_test("Byte Substitution", aes_key_addition);
+    run_test("Shift Rows", aes_key_addition);
+    run_test("Mix Columns", aes_key_addition);
+    run_test("Key Schedule", aes_key_addition);
+
+    end_tests();
+}
